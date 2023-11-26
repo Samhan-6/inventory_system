a4cebbc56fb2183ac5d0609829e33222e9589a78
Mohomed Ziyath <mohomed.ziyath1@gmail.com>
2023-11-26 10:16:36 +0530

    1. Added static path to settings.py 2. Corrected Supplier country label value

--- InventorySys/settings.py
+++ InventorySys/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,7 +82,7 @@ DATABASES = {
         'PORT': '',
         'NAME': 'IMS',
         'USER': 'sa',
-        'PASSWORD': '',
+        'PASSWORD': 'nim#MBI&ccr',
 
         'OPTIONS': {
             'driver': 'ODBC Driver 17 for SQL Server',
@@ -126,6 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
